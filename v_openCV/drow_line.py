4564b5c917c38622ea63047530e55572b4b2af43
JungTaehyan <erzangthehua@gmail.com>
2022-11-04 15:56:58 +0900

    선그리기

---
+++ v_openCV/drow_line.py
@@ -0,0 +1,13 @@
+import cv2
+
+img =cv2.imread('../v_openCV/imgs/whight.jpg')
+# cv2.line(img, start, end, color(BRG), thickness(픽셀단위), line Type)
+
+cv2.line(img, (50, 50), (150, 150), (255,0,0))
+cv2.line(img, (100, 350), (400, 400), (0,0,255), 20, cv2.LINE_4)
+if img is not None:
+    cv2.imshow("line", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else:
+    print("no imgs")
