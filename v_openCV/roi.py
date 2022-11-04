cb5988e81f789b2edd9bc23a5e3f8ea009530855
JungTaehyan <erzangthehua@gmail.com>
2022-11-04 18:03:25 +0900

    roi

--- v_openCV/roi.py
+++ v_openCV/roi.py
@@ -6,6 +6,10 @@ img = cv2.imread("../v_openCV/imgs/dog.jpg")
 x = 600; y = 520; w = 50; h = 50
 roi = img[y:y+h, x:x+w]
 
+img2 = roi.copy() # roi 배열 복제
+
+img[y:y+h, x+w:x+w+w] = roi # img에서 해당 영역을 roi로 채움!!
+
 print(roi.shape)
 cv2.rectangle(roi, (0,0), (h-1, w-1), (0,0,255))
 cv2.imshow("img", img)
