3d93a2abf839908cfe70e4d4545a325d333fe007
JungTaehyan <erzangthehua@gmail.com>
2022-11-04 09:49:43 +0900

    waitKey() 정의

--- v_openCV/read_video.py
+++ v_openCV/read_video.py
@@ -8,7 +8,11 @@ if cap.isOpened(): # 영상 객체 초기화, 주로 True/Fals로 반환됨.
         ret, img = cap.read() # 다음 프레임을 읽는 무한루프
         if ret:
             cv2.imshow(video_file, img) # 화면에 표시
-            cv2.waitKey(25) # 25ms 지연 (40fps으로 가정)
+            cv2.waitKey(25) # 25ms(밀리세컨즈) 지연 (40fps으로 가정) 키 입력을 기다리는 대기함수
+            # cv2.waitKey() -> 매개변수로 넘긴 시간 안에 키를 입력하면 소스의 다음줄로 이동.
+            # 만약 지정 시간 안에 키를 입력하지 않으면 넘어가게 되어있음
+            # 0:무한 대기, -1: 키가 눌리지 않을때, 아스키코드로 확인 가능
+            # 예시 : if cv2.waitKey() == ord('q'): # q를 누르면 종료
         else: #다음 프레임을 읽을 수 없을때
             break
 else:
