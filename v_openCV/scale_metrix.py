213534bf43e9d145b552c8b53b7373c4282d81c0
JungTaehyan <erzangthehua@gmail.com>
2022-11-09 10:06:16 +0900

    이미지 확대 축소

--- v_openCV/scale_metrix.py
+++ v_openCV/scale_metrix.py
@@ -6,30 +6,39 @@ import numpy as np
 img = cv2.imread('../img/photo.jpg')
 height, width = img.shape[:2]
 
-# 축소
-m_small = np.float32([[0.5, 0, 0],
-                       [0, 0.5,0]])  
-# 2배 확대
-m_big = np.float32([[2, 0, 0],
-                     [0, 2, 0]])  
 
-# 보간법 X
-dst1 = cv2.warpAffine(img, m_small, (int(height*0.5), int(width*0.5)))
-dst2 = cv2.warpAffine(img, m_big, (int(height*2), int(width*2)))
+# 변환 행렬 사용
+# # 축소
+# m_small = np.float32([[0.5, 0, 0],
+#                        [0, 0.5,0]])  
+# # 2배 확대
+# m_big = np.float32([[2, 0, 0],
+#                      [0, 2, 0]])  
 
-# 보간법 O
-dst3 = cv2.warpAffine(img, m_small, (int(height*0.5), int(width*0.5)), \
-                        None, cv2.INTER_AREA)
-dst4 = cv2.warpAffine(img, m_big, (int(height*2), int(width*2)), \
-                        None, cv2.INTER_CUBIC)
+# # 보간법 X
+# dst1 = cv2.warpAffine(img, m_small, (int(height*0.5), int(width*0.5)))
+# dst2 = cv2.warpAffine(img, m_big, (int(height*2), int(width*2)))
 
-# 보간법
+# # 보간법 O
+# dst3 = cv2.warpAffine(img, m_small, (int(height*0.5), int(width*0.5)), \
+#                         None, cv2.INTER_AREA)
+# dst4 = cv2.warpAffine(img, m_big, (int(height*2), int(width*2)), \
+#                         None, cv2.INTER_CUBIC)
+
+
+#  변환 행렬 사용 X
+# 크기 지정으로 축소
+#dst1 = cv2.resize(img, (int(width*0.5), int(height*0.5)),None, 0, 0, cv2.INTER_AREA)
+dst1 = cv2.resize(img, (int(width*0.5), int(height*0.5)), interpolation=cv2.INTER_AREA)
+
+# 배율 지정으로 확대
+dst2 = cv2.resize(img, None,  None, 2, 2, cv2.INTER_CUBIC)
 
 # 결과 출력
 cv2.imshow("original", img)
 cv2.imshow("small", dst1)
 cv2.imshow("big", dst2)
-cv2.imshow("small INTER_AREA", dst3)
-cv2.imshow("big INTER_CUBIC", dst4)
+# cv2.imshow("small INTER_AREA", dst3)
+# cv2.imshow("big INTER_CUBIC", dst4)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
